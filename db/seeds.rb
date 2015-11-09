e330489ef4706e940b8335e5d61d769edce51b68
Anna <alioubimova@yahoo.com>
2015-11-09 14:46:57 -0500

    seed reddit

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 # Examples:
 links = Link.create([{:title => "indiana flu death",:URL => "http://www.indystar.com/story/news/2015/10/28/indiana-sees-first-flu-death-year/74735682/"},
 {:title => "money and cars",:URL => "http://www.indystar.com/story/money/cars/2015/11/06/consumer-reports-reliablity-20--worst/75275986/"},
-{:title => "licence plate",:URL => "http://www.indystar.com/story/news/2015/11/06/supreme-court-personalized-license-plates-government-speech/75294426/"}
+{:title => "licence plate",:URL => "http://www.indystar.com/story/news/2015/11/06/supreme-court-personalized-license-plates-government-speech/75294426/"},
 {:title => "indiana bees",:URL => "http://www.indystar.com/story/life/home-garden/2015/11/06/hoosier-gardener-concerned-protecting-bees/75293956/"}
 
 ])
